a74e621b7cd3e468afd87590d250ce0bb777fd1f
sethdelrey <holdenbrown10@gmail.com>
2019-11-12 23:33:38 -0600

    - Working on HashTrie

--- src/trie/HashTrie.java
+++ src/trie/HashTrie.java
@@ -15,18 +15,19 @@ package trie;
 //        Use a chaining based global Hashtable. Use the number of lines N in WORD.LST as the size of Hashtable[N].
 //
 //        HashEntryNode will have following fields:
-//        {Node *parent,
-//        char c
-//        Node *child
-//        HashEntryNode *next
+//        {
+//              Node *parent,
+//              char c
+//              Node *child
+//              HashEntryNode *next
 //        }
-//
+
 //        The Hashtable is an array of HashEntryNode* Hashtable[N] .
 //        Hash function will take two values as an input Node *parent and char c and calculate hash value between 0.. N-1.
 
-
 public class HashTrie {
     public static HashEntryNode[] hashTable;
+    public static int idCounter;
 
     private static class HashEntryNode {
         Node parent;
@@ -76,44 +77,193 @@ public class HashTrie {
 
         int nodeId;
 
-        public Node() {
-            // default constructor
+        public Node(char lead, String label, boolean isWord) {
+            this.lead = lead;
+            this.label = label;
+            this.isWord = isWord;
         }
 
-        public Node(char _lead, String _label, boolean _isWord) {
-            lead = _lead;
-            label = _label;
-            isWord = _isWord;
+        public Node findChild(char c) {
+            if (firstChild == null) {
+                return null;
+            }
+
+            int hashCode = hashCode(this.firstChild, c);
+            HashEntryNode current = hashTable[hashCode];
+            while (current != null) {
+                if (current.c == c) {
+                    break;
+                }
+                current = current.next;
+            }
+            return (current != null) ? current.child : null;
         }
 
         public int hashCode(Node parent, char _c) {
-            char parentC = (parent != null) ? parent.lead : _c;
+            int parentC = (parent != null) ? parent.nodeId : _c;
             int hash = _c * 5 + parentC * 7;
             hash = hash % hashTable.length;
             return hash;
         }
 
+//        public Node findChild(char c) {
+//            if (firstChild == null) {
+//                return null;
+//            }
+//            else {
+//                Node iterator = firstChild;
+//                while (iterator.rightSibling != null && iterator.rightSibling.lead <= c) {
+//                    iterator = iterator.rightSibling;
+//                }
+//
+//                return iterator;
+//            }
+//        }
 
+        public void insert(Node newNode) {
+            Node temp = findChild(newNode.lead);
+            insertIntoHashTable(newNode.hashCode(this, newNode.lead),this,newNode,newNode.lead);
+
+            if (temp == null) {
+                firstChild = newNode;
+            }
+            else if (temp.lead < newNode.lead) {
+                newNode.rightSibling = temp.rightSibling;
+                temp.rightSibling = newNode;
+            }
+            else if (temp.lead == newNode.lead) {
+                char[] wordAsArray = newNode.label.toCharArray();
+                char[] labelAsArray = temp.label.toCharArray();
+                int i = 0;
+                if (wordAsArray.length >= labelAsArray.length) {
+                    for (i = 0; i < labelAsArray.length; i++) {
+                        if (wordAsArray[i] != labelAsArray[i]) {
+                            break;
+                        }
+                    }
+                }
+                else {
+                    for (i = 0; i < wordAsArray.length; i++) {
+                        if (wordAsArray[i] != labelAsArray[i]) {
+                            break;
+                        }
+                    }
+                }
+
+                // Split label at i and form two nodes.
+                if (i == labelAsArray.length && labelAsArray.length == wordAsArray.length) {
+                }
+                else {
+                    String newOriginalLabel = temp.label.substring(0,i);
+                    String newLabel1 = temp.label.substring(i);
+                    String newLabel2 = newNode.label.substring(i);
+                    temp.label = newOriginalLabel;
+                    if (newLabel1.length() != 0 && newLabel2.length() != 0) {
+                        char newLead1 = newLabel1.charAt(0);
+                        char newLead2 = newLabel2.charAt(0);
+                        newLabel1 = (newLabel1.length() == 1) ? "" : newLabel1.substring(1);
+                        newLabel2 = (newLabel2.length() == 1) ? "" : newLabel2.substring(1);
+                        Node nodeForOriginalNode = new Node(newLead1, newLabel1, temp.isWord);
+                        newNode.lead = newLead2;
+                        newNode.label = newLabel2;
+                        temp.isWord = false;
+                        if (nodeForOriginalNode.lead < newNode.lead) {
+                            nodeForOriginalNode.firstChild = temp.firstChild;
+                            temp.firstChild = nodeForOriginalNode;
+                            nodeForOriginalNode.rightSibling = newNode;
+                        }
+                        else {
+                            newNode.firstChild = temp.firstChild;
+                            temp.firstChild = newNode;
+                            newNode.rightSibling = nodeForOriginalNode;
+                        }
+                    }
+                    else if (newLabel1.length() != 0) {
+                        Node nodeForOriginalNode = new Node(newLabel1.charAt(0), newLabel1, true);
+                        temp.firstChild = nodeForOriginalNode;
+                    }
+                    else {
+                        newNode.lead = newLabel2.charAt(0);
+                        newNode.label = (newLabel2.length() == 1) ? "" : newLabel2.substring(1);
+                        temp.insert(newNode);
+                    }
+                }
+            }
+            else if (temp == firstChild) {
+                newNode.rightSibling = firstChild;
+                firstChild = newNode;
+            }
+        }
+
+        public void display(String str) {
+            str += lead + label;
+            if (isWord) {
+                System.out.println(str);
+            }
+            if (firstChild != null) {
+                firstChild.display(str);
+            }
+            if (rightSibling != null) {
+                str = str.substring(0,str.length()-(1+label.length()));
+                rightSibling.display(str);
+            }
+        }
     }
 
-    private HashEntryNode root;
+
+    private Node root;
 
     public HashTrie(int size) {
         hashTable = new HashEntryNode[size];
+        root = new Node(' ', null, false);
+    }
+
+    public static void insertIntoHashTable(int _hashCode, Node _parent, Node _child, char _c) {
+        HashEntryNode eNode = hashTable[_hashCode];
+
+        HashEntryNode newNode = new HashEntryNode() {
+            {
+                parent = _parent;
+                child = _child;
+                c = _c;
+                next = (eNode != null) ? eNode.next : null;
+            }
+        };
+
+        if (eNode != null) {
+            eNode.next = newNode;
+        }
+        else {
+            hashTable[_hashCode] = newNode;
+        }
     }
 
     public void insert(String newWord) {
         char letter = newWord.charAt(0);
-        HashEntryNode node = root.findChild(letter);
-        HashEntryNode newNode = new HashEntryNode(letter);
+        newWord = (newWord.length() == 1) ? "" : newWord.substring(1);
+        Node newNode = new Node(letter, newWord, true);
+        root.insert(newNode);
 
-        if (node == null) {
-            hashTable[newNode.hashCode(letter)] = newNode;
+        idCounter++;
+    }
+
+    public void lookup(String prefix) {
+        if (prefix != null && !prefix.isEmpty()) {
+            char[] prefixAsArray = prefix.toCharArray();
+            Node current = root.findChild(prefixAsArray[0]);
+            Node parent = root;
+            int i = 0;
+            while (i < prefix.length() && current != null) {
+                if (current.lead == prefixAsArray[i]) {
+                    parent = current;
+                    current = current.firstChild;
+                    i++;
+                } else {
+                    current = current.rightSibling;
+                }
+            }
+
+            current.display(prefix);
         }
-        else {
-            
-        }
-
-
     }
 }
